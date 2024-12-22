cd4309d79a421e505f1af22455e5779616007b5c
Hunter <egorkholkin2018@gmail.com>
2024-12-22 15:57:38 +0300

    Start Fix

--- Body/Start.py
+++ Body/Start.py
@@ -4,10 +4,10 @@ import subprocess
 Project_Path = os.path.dirname(__file__)
 
 def lang_var():
-    return open(f"{Project_Path}\lang_var.txt", 'r', encoding='utf-8').read() 
+    return open(rf"{Project_Path}\lang_var.txt", 'r', encoding='utf-8').read() 
 
 if lang_var() == "ru":
-    subprocess.Popen(['python', ".\Body\RemoteRU.py"])
+    subprocess.Popen(['python', r".\Body\RemoteRU.py"])
 
 elif lang_var() == "en":
-    subprocess.Popen(['python', ".\Body\RemoteEN.py"])
+    subprocess.Popen(['python', r".\Body\RemoteEN.py"])
